3709f21087240d0102e77300fb9fcb3b049f6537
khosrora <khosrora153333@gmail.com>
2024-07-03 03:38:24 -0400

    test deploy 1

--- src/app/(public)/critics/Form.jsx
+++ src/app/(public)/critics/Form.jsx
@@ -2,17 +2,31 @@
 import ButtonCu from "@/app/components/common/ButtonCu";
 import InputCu from "@/app/components/common/InputCu";
 import TextareaCu from "@/app/components/common/TextareaCu";
+import { postDataAPI } from "@/utils/get_data";
 import React from "react";
 import { useForm } from "react-hook-form";
+import { toast } from "sonner";
 
 function Form() {
   const {
     register,
     handleSubmit,
     watch,
+    reset , 
     formState: { errors },
   } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = async (data) =>  {
+    try {
+        const {status} = await postDataAPI(['siteinfo/critic/'] , data);
+
+      if(status === 201) {
+        toast.success("پیشنهاد شما با موفقیت ارسال شد")
+        reset()
+      }
+    } catch (error) {
+      toast.error("مجدد امتحان کنید")
+    }
+  };
 
   console.log(watch("example"));
 
@@ -21,16 +35,16 @@ function Form() {
   <InputCu
       title="نام و نام خانوادگی "
       placeholder="نام و نام خانوادگی خود را وارد نمایید"
-      name="firstName"
-      validation={...register("firstName" , { required : {value : true , message : 'این فیلد الزامی است' } })}
-      errorMessage={errors?.firstName?.message}
+      name="full_name"
+      validation={...register("full_name" , { required : {value : true , message : 'این فیلد الزامی است' } })}
+      errorMessage={errors?.full_name?.message}
     />
   <InputCu
       title="شماره موبایل"
       placeholder="شماره موبایل خود را وارد نمایید"
-      name="phone_number"
-      validation={...register("phone_number" , { required : {value : true , message : 'این فیلد الزامی است' } })}
-      errorMessage={errors?.phone_number?.message}
+      name="phone"
+      validation={...register("phone" , { required : {value : true , message : 'این فیلد الزامی است' } })}
+      errorMessage={errors?.phone?.message}
     />
   <InputCu
       title="پست الکترونیک"
@@ -42,9 +56,9 @@ function Form() {
  <TextareaCu
     title="متن پیام"
     placeholder="متن پیام خود را وارد نمایید"
-    name="desc"
-    validation={...register("desc" , { required : {value : true , message : 'این فیلد الزامی است' } })}
-    errorMessage={errors?.desc?.message}
+    name="text"
+    validation={...register("text" , { required : {value : true , message : 'این فیلد الزامی است' } })}
+    errorMessage={errors?.text?.message}
     />
     <ButtonCu type="submit" title='ثبت درخواست'  />
   </form>
