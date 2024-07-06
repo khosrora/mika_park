c9255e0aa0b2fe5c300c5aad11c4b3005e2f206b
khosrora <khosrora153333@gmail.com>
2024-07-06 03:25:43 -0400

    add base api /\

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["45.156.186.124" , "mikapark"]
+        domains: ["45.156.186.124" , "mikapark" , "api.mikapark.ir"]
     }
 };
 
