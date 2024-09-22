674f91a87e1a2f128657b1b4bb4d762a8d504d10
khosrora <khosrora153333@gmail.com>
2024-09-22 12:16:56 -0400

    add mega header 3

--- src/app/MegaVideoSection.jsx
+++ src/app/MegaVideoSection.jsx
@@ -4,8 +4,6 @@ function MegaVideoSection({ details }) {
   return (
     <div className="p-4">
       <header className="relative flex items-center justify-center h-64 lg:h-screen mb-4 overflow-hidden rounded-md">
-        {/* <div class="relative z-30  text-2xl text-white bg-zinc-950 w-full h-full bg-opacity-30 rounded-xl">
-      </div> */}
         <video
           controls
           autoPlay
