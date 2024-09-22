f299206b3bcedc436806ab85c5b41f53c6954f2a
khosrora <khosrora153333@gmail.com>
2024-09-22 12:19:35 -0400

    ready for deploy

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -20,7 +20,6 @@ export default async function Home() {
         <MegaVideoSection details={data} />
       </Suspense>
       <Header details={data} />
-      {/* <ServicesMikaPark /> */}
       <BigProjectSection details={data} />
       <PossibilitiesSection />
       <OtherProjects />
